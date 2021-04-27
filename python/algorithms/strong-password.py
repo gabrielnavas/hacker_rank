aa1b4ee1af439bb49cfa8882e6fdfdaee76f23de
Navas <gabrielnavas@email.com>
2021-04-27 00:13:58 -0300

    strong-password

---
+++ python/algorithms/strong-password.py
@@ -0,0 +1,10 @@
+def minimumNumber(n, password):
+	numbers = "0123456789"
+	lower_case = "abcdefghijklmnopqrstuvwxyz"
+	upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	special_characters = "!@#$%^&*()-+"
+
+	
+	for n in numbers:
+		if n
+  
